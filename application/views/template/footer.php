cccf979d00a1dbcbba87fd262709660c7fbd1cbe
anindyap <nindyaprbsr@gmail.com>
2025-05-15 22:25:35 +0700

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+<!-- Scripts -->
+<script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.scrolly.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/browser.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/breakpoints.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/util.js'); ?>"></script>
+<script src="<?= base_url('assets/js/main.js'); ?>"></script>
+
+</body>
+
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Anindya WebPro</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/main.css'); ?>" />
+</head>
+
+<body class="is-preload">
+
+    <!-- Nav -->
+    <nav id="nav">
+        <ul class="container">
+            <li><a href="#top">Top</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
